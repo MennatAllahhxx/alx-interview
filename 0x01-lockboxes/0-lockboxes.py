88c9538e0048d5be34763d6a4d742137384dbbfa
Menna Hassan <h.menna85@gmail.com>
2024-03-15 04:06:48 +0200

    task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Module contains canUnlockAll and boxUnlocked funs
+"""
+
+
+def canUnlockAll(boxes):
+    """AI is creating summary for canUnlockAll
+
+    Args:
+        boxes (List[List[int]]): a list of lists
+
+    Returns:
+        bool: True if all boxes can be opened, else return False
+    """
+    num_of_boxes = len(boxes)
+    checklist = [0 for box in range(num_of_boxes)]
+    checklist[0] = 1
+
+    checklist_updated = boxUnlocked(boxes, boxes[0], checklist)
+
+    for check in checklist_updated:
+        if (check == 0):
+            return False
+    return True
+
+
+def boxUnlocked(boxes, keys_inside, checklist):
+    """AI is creating summary for boxUnlocked
+
+    Args:
+        boxes (List[List[int]]): a list of lists
+        keys_inside (List[int]): a list of keys
+        checklist (List): a list of int
+
+    Returns:
+        Checklist: returns checklist after update
+    """
+    for key_inside in keys_inside:
+        if (checklist[key_inside]):
+            return checklist
+
+        checklist[key_inside] = 1
+        boxUnlocked(boxes, boxes[key_inside], checklist)
+    return checklist
