a8b4dcf89caca8eb493de13a02a3624ca729d340
Menna Hassan <h.menna85@gmail.com>
2024-03-22 03:34:00 +0200

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,29 +13,22 @@ def minOperations(n):
     Returns:
         int: least number of operations.
     """
-    if n <= 1:
+    if not isinstance(n, int) or n <= 1:
         return 0
 
-    # Set the minimum operation variable to 0
-    min_operations = 0
-    # Set the current length variable to 1 as we start with 1 character 'H'
-    current_length = 1
-    # Set the clipboard variable to 0 as we are not copying anything yet
-    clipboard = 0
+    ops = 0
+    copy_value = 0
+    items = 1
+    left = n - items
 
-    # Loop until current length is equal to n
-    while current_length < n:
-        # If n is divisible by current length, we can copy all
-        if n % current_length == 0:
-            # This is the only time we can copy
-            clipboard = current_length
-            # Minimum operation is incremented by 1 because we copied
-            min_operations += 1
+    while left > 0:
+        left = n - items
+        if n % items == 0:
+            ops = ops + 1
+            copy_value = items
 
-        # Paste the clipboard
-        current_length += clipboard
-        # Minimum operation is incremented by 1 because we pasted
-        min_operations += 1
+        items = items + copy_value
+        ops = ops + 1
+        left = n - items
 
-    # Return minimum operation
-    return min_operations
+    return ops
