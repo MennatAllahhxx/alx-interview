c17db5c5acb4b888b16be0f45656fac8b67c307a
Menna Hassan <h.menna85@gmail.com>
2024-04-08 02:27:37 +0200

    task 0

---
+++ 0x04-utf8_validation/0-main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+validUTF8 = __import__('0-validate_utf8').validUTF8
+
+data = [65]
+print(validUTF8(data))
+
+data = [240, 144, 128, 128]
+print(validUTF8(data))
+
+data = [226, 130, 172]
+print(validUTF8(data))
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
+
+data = [192, 128]
+print(validUTF8(data))
+
+data = [128, 128, 128]
+print(validUTF8(data))
+
+data = [255]
+print(validUTF8(data))
+
+data = [192, 1]
+print(validUTF8(data))
---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Module contains validUTF8 fun
+"""
+
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """AI is creating summary for validUTF8
+
+    Args:
+        data (List[int]): data set to be checked
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else return False.
+    """
+    bytes_num = len(data)
+    if bytes_num == 0:
+        return True
+
+    i = 0
+
+    while i < bytes_num:
+        ones_num = 0
+        if (data[i] >> 7) & 1:
+            ones_num = get_ones(data[i])
+            for j in range(ones_num):
+                if (j+ i + 2) > bytes_num or\
+                   (((data[i + j + 1] >> 7) & 1) == 0) or\
+                   (((data[i + j + 1] >> 6) & 1) == 1):
+                    return False
+        i += ones_num + 1
+    return True
+
+
+def get_ones(number: int) -> int:
+    """AI is creating summary for get_ones
+
+    Args:
+        number (int): number to be checked
+
+    Returns:
+        int: number of ones
+    """
+    num = 0
+
+    while number & (1<< 7):
+        num += 1
+        number = number << 1
+
+    return num - 1
