15eb9a6ca90214865183557a2b516070c15fb767
Menna Hassan <h.menna85@gmail.com>
2024-04-26 05:08:18 +0300

    task 0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,16 +5,23 @@ Rotate 2D Matrix
 
 from typing import List
 
-def rotate_2d_matrix(matrix: List[List[int]]) ->  List[List[int]]:
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> List[List[int]]:
+    """AI is creating summary for rotate_2d_matrix
+
+    Args:
+        matrix (List[List[int]]): [description]
+
+    Returns:
+        List[List[int]]: [description]
+    """
     n = len(matrix)
-    
-    # Transpose the matrix
+
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
-    # Reverse each row
+
     for i in range(n):
         matrix[i].reverse()
-    
+
     return matrix
